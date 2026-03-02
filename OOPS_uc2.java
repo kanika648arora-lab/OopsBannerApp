8154756cb507f715ffeab6a11e2373830cfa7344
Kanika Arora <kanika648arora@gmail.com>
2026-03-02 19:28:24 +0530

    Add files via upload

---
+++ OOPS_uc2.java
@@ -0,0 +1,12 @@
+public class OOPS_uc2 
+{
+    public static void main(String[] args) {
+
+        System.out.println(" **   **   **   *** ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * **   *** ");
+        System.out.println("*     * *     * *            * ");
+        System.out.println(" **   **  *       ***  ");
+
+    }
+}
